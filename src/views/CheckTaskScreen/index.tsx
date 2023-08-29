8c596184576160e60e141da1f1f99f8526e297e5
ndhphuc <ndanghoangphuc@gmail.com>
2023-08-29 19:01:28 +0700

    update 1

---
+++ src/views/CheckTaskScreen/index.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export default function CheckTaskScreen() {
+  return (
+    <View>
+      <Text>ádasdasdasdaczxcz</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({});
